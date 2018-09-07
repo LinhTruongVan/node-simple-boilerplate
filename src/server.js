337300cdfba33d0a033ce5e861422336902f561c
Linh Truong <linh.truong@codelink.io>
2018-09-07 17:34:04 +0700

    Setup winston logger

--- src/server.js
+++ src/server.js
@@ -1,1 +1,3 @@
-console.log('Hello world!');
+import logger from './utils/logger';
+
+logger.info('Hello World!');
---
+++ src/utils/logger.js
@@ -0,0 +1,18 @@
+import winston from 'winston';
+import 'winston-daily-rotate-file';
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.DailyRotateFile({
+      filename: './logs/%DATE%.log',
+      datePattern: 'YYYY-MM-DD-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d'
+    })
+  ]
+});
+
+export default logger;
